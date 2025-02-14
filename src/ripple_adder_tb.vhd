272505570b47fb1aeea4b10d4c0b080c0289fde4
c27keelieohollaren <c27keelie.o'hollaren@afacademy.af.edu>
2025-02-13 22:19:38 -0700

    Update ripple_adder_tb.vhd

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -47,9 +47,14 @@ begin
 	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
+	   w_addends <= x"5A"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with 5A" severity failure;
+	   w_addends <= x"FF"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
+	   
        -- TODO, a few other test cases
-	
+	   	
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
